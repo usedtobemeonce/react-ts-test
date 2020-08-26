daecbbdb51ef8a23bc163d23a3a7ff08a8ae9a3c
usedtobemeonce <usedtobemeonce@gmail.com>
2020-08-26 23:24:33 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid } from './components/Grid';
+import { ReducerExample } from './ReducerExample';
+import { Counter } from './components/Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <div>
+    <Grid text="hello" person={ {firstName: 'bob', lastName: 'deen' }} />
+
+    <br/>
+    <br/>
+    <br/>
+
+    <ReducerExample />
+
+    <Counter>
+      {({ count, setCount }) => (
+        <div>
+          {count}
+          <button onClick={() => setCount(count + 1)}>+</button>
+        </div>
+      )}
+    </Counter>
+  </div>;
+};
 
 export default App;
+ 
